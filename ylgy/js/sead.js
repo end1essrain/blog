3294aaf3be3772339d7d6d71327a323598f47e54
lesevan <baidu2xia@hotmail.com>
2022-09-19 06:28:02 +0800

    Site updated: 2022-09-19 06:28:01

--- ylgy/js/sead.js
+++ ylgy/js/sead.js
@@ -1,10 +1,10 @@
  layer.confirm(
-      "欢迎访问羊羊通关神器<br>更多福利请关注微信公众号[红贝]",
+      "欢迎访问咩咩刷通关<br>更多福利请关注微信公众号[红贝]",
       {
-        btn: ["现在加入", "好的"]
+        btn: ["现在关注", "好的"]
       },
       function () {
-        window.open("../img/wx.jpg", "__blank");
+        window.open("./img/wx.jpg", "__blank");
       },
       function () {}
     );
